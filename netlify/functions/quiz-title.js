4a1a575a3a0aeb92edd5f350332ff92e80c790db
NihalBenarbiya <benarbiyanihal2@gmail.com>
2026-06-01 23:52:27 +0100

    fix quiz titre

--- netlify/functions/quiz-title.js
+++ netlify/functions/quiz-title.js
@@ -23,7 +23,7 @@ exports.handler = async (event) => {
 
     return {
       statusCode: 200,
-      headers: { "Content-Type": "application/json", "Cache-Control": "max-age=3600" },
+     headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ title: title || "Quiz" }),
     };
 
